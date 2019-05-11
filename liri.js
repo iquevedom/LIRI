c3197f7d6a25b8468153458adfa10c59fcc0b83a
Isidro Quevedo <isidroquevedo@hotmail.com>
2019-05-10 19:21:42 -0700

    Successful venue API

--- liri.js
+++ liri.js
@@ -1,91 +1,69 @@
+// Packages load
 require("dotenv").config();
 var keys = require("./keys.js");
 //var spotify = new Spotify(keys.spotify);
 
-
-var moment=require("moment");
-
+var moment = require("moment");
+var inquirer = require("inquirer");
 var axios = require("axios");
 
 var option = process.argv[2];
 var selection = process.argv[3];
 
-switch (option) {
-    case "concert-this":
-        concierto(selection);
-        break;
-    case "spotify-this-song":
-        musica(selection);
-        break;
-    case "movie-this":
-        movies(selection);
-        break;
-    case "do-what-it-says":
-        random(selection);
-        break;
-    default:
-        break;
-}
 
-// shaws
-function concierto(selection) {
-    console.log(option);
-     selection = "Paul McCartney";
-    console.log(selection);
-    var urlConcierto = "https://rest.bandsintown.com/artists/" + selection + "/events?app_id=codingbootcamp";
-    console.log(urlConcierto);
-    axios.get(urlConcierto).then(
+// Create a "Prompt" with a series of questions.
+inquirer
+    .prompt([
+        // Here we create a basic text prompt.
+        {
+            type: "list",
+            message: "Please choose your option :",
+            choices: ["concert-this", "spotify-this-song", "movie-this", "do-what-it-says"],
+            name: "userSelection",
+        },
+
+        {
+            type: "input",
+            message: "Enter your search :",
+            name: "userSearch",
+        }
+    ])
+    .then(function (inquirerResponse) {
+        // If the inquirerResponse confirms, we displays the inquirerResponse's username and pokemon from the answers.
+        switch (inquirerResponse.userSelection) {
+            case "concert-this":
+                venue(inquirerResponse.userSelection);
+                break;
+            case "spotify-this-song":
+                /*       musica(selection); */
+                break;
+            case "movie-this":
+                /*   movies(selection); */
+                break;
+            case "do-what-it-says":
+                /*   random(selection); */
+                break;
+            default:
+                break;
+        }
+    });
+
+function venue(selection) {
+    var selection = "Paul McCartney";
+    var urlVenue = "https://rest.bandsintown.com/artists/" + selection + "/events?app_id=codingbootcamp";
+    axios.get(urlVenue).then(
         function (response) {
             // If the axios was successful...
             // Then log the body from the site!
-           /*  console.log(response.data); */
-
+            /*  console.log(response.data); */
+console.log("\n",selection, "Events-----\n")
             for (let i = 0; i < response.data.length; i++) {
-                console.log(response.data[i].venue.country);
-                console.log(response.data[i].venue.city);
-                console.log(response.data[i].venue.name);
-                console.log(moment(response.data[i].datetime).format("MM/DD/YYYY"));
-                
+                console.log("Country : ",response.data[i].venue.country);
+                console.log("City : ",response.data[i].venue.city);
+                console.log("Name : ",response.data[i].venue.name);
+                console.log("Date : ", moment(response.data[i].datetime).format("MM/DD/YYYY"),"\n");
+
             }
 
-        },
-
-
-        
-        function (error) {
-            if (error.response) {
-                // The request was made and the server responded with a status code
-                // that falls out of the range of 2xx
-                console.log("error.response.data");
-                console.log(error.response.status);
-                console.log(error.response.headers);
-            } else if (error.request) {
-                // The request was made but no response was received
-                // `error.request` is an object that comes back with details pertaining to the error that occurred.
-                console.log(error.request);
-            } else {
-                // Something happened in setting up the request that triggered an Error
-                console.log("Error", error.message);
-            }
-            console.log(error.config);
-        }
-
-
-
-    );
-
-}
-
-function musica(selection) {
-
-
-}
-function movies(selection) {
-
-
-}
-
-function random(selection) {
-
-
-}
+        });
+};
